647dcf51232d559cf8ba5ae72824f03360b731c8
Alexandre Spindola <alexandre.spindola11@gmail.com>
2024-03-15 18:40:04 +0100

    Atualizações no DatabaseSeeder.php: Habilita o uso do UsersSeeder e desabilita o uso do ContactsTableSeeder.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,8 +11,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            ContactsTableSeeder::class,
-            // UsersSeeder::class,
+            // ContactsTableSeeder::class,
+            UsersSeeder::class,
         ]);
     }
 }
